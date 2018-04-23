787f90fbb1acdcf46b4f1dcd4f94e18a7c2d9619
aghebert <30908336+aghebert@users.noreply.github.com>
2018-04-23 18:31:02 -0400

    Added HTML to sort

--- lab4/assets/header.php
+++ lab4/assets/header.php
@@ -13,7 +13,8 @@
 <head>
     <meta charset="UTF-8">
     <title>Assignment 3 - Aaron Hebert</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
     <nav></nav>
 </head>
 <body>
