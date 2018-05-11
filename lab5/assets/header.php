3cd1430a780633bcecb00cab23afe92f9cbe3163
aghebert <30908336+aghebert@users.noreply.github.com>
2018-05-11 17:29:50 -0400

    Basic layout
    added siteEntry and sitesLookup.
    Modified some other pages values

--- lab5/assets/header.php
+++ lab5/assets/header.php
@@ -13,9 +13,10 @@
 <head>
     <meta charset="UTF-8">
     <title>Lab 5 - Aaron Hebert</title>
-    <link href="css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     <link href="css/custom.css" rel="stylesheet">
-    <nav></nav>
+
+    <nav class="nav-justified"><a href="index.php">Site Entry</a> | <a href="list.php">Site Listing</a></nav>
 </head>
 <body>
 <section id="mainpage">
--- lab5/index.php
+++ lab5/index.php
@@ -5,3 +5,12 @@
  * Date: 5/11/18
  * Time: 4:03 PM
  */
+
+require_once("assets/dbconn.php");
+include_once("assets/header.php");
+
+include_once("assets/siteEntry.php");
+include_once("assets/sitesLookup.php");
+
+
+include_once ("assets/footer.php");
