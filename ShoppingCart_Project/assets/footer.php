29832bd56f5a53c94999953d3bab44d67085cf2f
aghebert <30908336+aghebert@users.noreply.github.com>
2018-05-26 15:28:21 -0400

    more js fix

--- ShoppingCart_Project/assets/footer.php
+++ ShoppingCart_Project/assets/footer.php
@@ -13,6 +13,6 @@
     Copyright &copy; <?php echo date('Y'); ?> Aaron Hebert. All rights reserved
 </footer>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="../js/bootstrap.bundle.js" type="text/javascript"></script>
+<script src="./js/bootstrap.bundle.js" type="text/javascript"></script>
 </body>
 </html>
--- ShoppingCart_Project/assets/header.php
+++ ShoppingCart_Project/assets/header.php
@@ -15,8 +15,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <title>Lab 5 - Aaron Hebert</title>
-    <link href="../css/bootstrap.css" rel="stylesheet">
-    <link href="../css/custom.css" rel="stylesheet">
+    <link href="./css/bootstrap.css" rel="stylesheet">
+    <link href="./css/custom.css" rel="stylesheet">
 
 </head>
 <body>
