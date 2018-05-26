adba1e1f4435fe089234d057c7c32ba84b8eb7d0
aghebert <30908336+aghebert@users.noreply.github.com>
2018-05-26 17:31:36 -0400

    Update 1

---
+++ ShoppingCart_Project/assets/login_register.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aaronhebert
+ * Date: 5/26/18
+ * Time: 5:19 PM
+ */
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aaronhebert
+ * Date: 5/26/18
+ * Time: 4:28 PM
+ */
+?>
+
+
+<form action="action">
+    <div class="container">
+        <label for="uname"><b>Username</b></label>
+        <input type="text" placeholder="Enter Username" name="uname" required>
+
+        <br>
+
+        <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" required>
+
+
+    </div>
+
+    <div class="container">
+        <button type="button" class="cancelbtn">Cancel</button>
+        <button type="submit">Login</button>
+    </div>
+</form>
+
+
+<form class="form-group" action="index.php">
+    <div class="container">
+        <h1>Sign Up</h1>
+        <p>Please fill in this form to create an account.</p>
+        <hr>
+
+        <label for="email"><b>Email</b></label>
+        <input type="text" placeholder="Enter Email" name="email" required>
+
+        <br/>
+
+        <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" required>
+
+        <br/>
+
+        <label for="psw-repeat"><b>Repeat Password</b></label>
+        <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+
+        <br/>
+
+        <div class="clearfix">
+            <button type="button" class="cancelbtn">Cancel</button>
+            <button type="submit" class="signupbtn">Sign Up</button>
+        </div>
+    </div>
+</form>
--- ShoppingCart_Project/index.php
+++ ShoppingCart_Project/index.php
@@ -9,24 +9,20 @@
  */
 
 
-
-
 require_once("assets/dbconn.php");
 
 
 include_once("assets/header.php");
 
 
-//include_once("assets/sitesLookup.php");
-
 //connect to database
 $db = dbConn();
-
+//$user_key = $_SESSION['user_Key'];
 
 
 // get the data from user, if any, and sanitize it.
 $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING) ??
-    filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING) ?? NULL;
+    filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING) ?? null;
 $searchTerm = filter_input(INPUT_GET, 'term', FILTER_SANITIZE_URL) ?? filter_input(INPUT_POST, 'term', FILTER_SANITIZE_URL) ?? null;
 $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT) ?? filter_input(INPUT_POST, 'id', FILTER_VALIDATE_INT) ?? null;
 
@@ -34,7 +30,21 @@ $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT) ?? filter_input(INPUT_P
 switch ($action) {
 
 
-    case "":
+    case "ADMIN_LOGIN":
+        /*
+        if($registered_user == true && $admin_rights == true){
+
+        }else{*/
+        echo "<h1>Administrator Login / Registration</h1>";
+        include_once("assets/login_register.php");
+        //}
+
+        break;
+
+    case 'LOGIN':
+        echo "<h1>Login / Registration</h1>";
+        include_once("assets/login_register.php");
+        break;
 
 
     DEFAULT:
